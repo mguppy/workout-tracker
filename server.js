67eef9eec9981826be02ca6d5a6f77a9ab913b21
mguppy <mguppy34@gmail.com>
2021-08-17 16:39:04 -0500

    connecting heroku and mongo atlas

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // calls route files
